c335097769bbd33ea13ff62bff8fca26ee5a5fd7
James Dudley <jedudley@outlook.com>
2023-04-29 13:47:44 -0500

    formatting tweaks

--- assets/js/SRCDS.CSGO.js
+++ assets/js/SRCDS.CSGO.js
@@ -13,17 +13,7 @@ SRCDS.CSGO.MapsDeathmatch = [ "de_dust2", "de_inferno", "de_mirage", "de_cbble",
 
 SRCDS.CSGO.MapsTest = ["clienttest-pillar", "poolday"];
 
-SRCDS.CSGO.MapsGet5 = ["de_dust2", "de_inferno", "de_nuke", "de_mirage", "de_overpass", "de_train", "de_vertigo"];
-
-
-"de_inferno", "de_nuke", "de_vertigo", "de_mirage", "de_overpass", "de_ancient", "de_anubis"
-
-
-
-
-
-
-
+SRCDS.CSGO.MapsGet5 = ["de_inferno", "de_nuke", "de_vertigo", "de_mirage", "de_overpass", "de_ancient", "de_anubis"];
 
 SRCDS.CSGO.LaunchArmsRace = function (hostname, map, ip) {
     "use strict";
